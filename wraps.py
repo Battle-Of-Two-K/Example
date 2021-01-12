d48b1fe34cd0586009b64eb6131fbc7aa7ed5a35
Battle-Of-Two-K <71852823+Battle-Of-Two-K@users.noreply.github.com>
2021-01-12 20:01:31 +0300

    Add files via upload

---
+++ wraps.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('400x400')
+
+n = 0
+
+def CLick():
+	global n
+	button.place_info()
+	move = 5
+	n += move
+	button.place(x = n, y = n)
+	button.after(100, CLick)
+
+
+
+button = Button(root, text="CLick", command=CLick, width=5, height=4)
+button.place(x = 0, y = 0)
+
+root.mainloop()
