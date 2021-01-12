c8ca63693d23673c57fed87d505de7ea17b27100
simensgreen <simensgreen007@gmail.com>
2021-01-12 20:12:36 +0300

    refactor

--- wraps.py
+++ wraps.py
@@ -5,17 +5,17 @@ root.geometry('400x400')
 
 n = 0
 
-def CLick():
+def click():
 	global n
 	button.place_info()
 	move = 5
 	n += move
 	button.place(x = n, y = n)
-	button.after(100, CLick)
+	button.after(100, click)
 
 
 
-button = Button(root, text="CLick", command=CLick, width=5, height=4)
+button = Button(root, text="CLick", command=click, width=5, height=4)
 button.place(x = 0, y = 0)
 
 root.mainloop()
